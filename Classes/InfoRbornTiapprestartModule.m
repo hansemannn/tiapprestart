43276dbd6f26d4d099e6947d19417b1879ebc907
hansemannn <h.knoechel@lambus.com>
2021-03-14 12:10:06 +0100

    chore: update to latest SDK

--- Classes/InfoRbornTiapprestartModule.m
+++ Classes/InfoRbornTiapprestartModule.m
@@ -26,48 +26,7 @@
 	return @"info.rborn.tiapprestart";
 }
 
-#pragma mark Lifecycle
-
--(void)startup
-{
-	// this method is called when the module is first loaded
-	// you *must* call the superclass
-	[super startup];
-	
-	NSLog(@"[INFO] %@ loaded",self);
-}
-
--(void)shutdown:(id)sender
-{
-	// this method is called when the module is being unloaded
-	// typically this is during shutdown. make sure you don't do too
-	// much processing here or the app will be quit forceably
-	
-	// you *must* call the superclass
-	[super shutdown:sender];
-}
-
-#pragma mark Cleanup 
-
--(void)dealloc
-{
-	// release any resources that have been retained by the module
-	[super dealloc];
-}
-
-#pragma mark Internal Memory Management
-
--(void)didReceiveMemoryWarning:(NSNotification*)notification
-{
-	// optionally release any resources that can be dynamically
-	// reloaded once memory is available - such as caches
-	[super didReceiveMemoryWarning:notification];
-}
-
 #pragma Public APIs
-
-
-
 
 -(void)restartApp:(id)unused
 {
